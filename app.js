04c4f3aefafa72a6524bcdedf1194d037b3a681d
Geoffroy Lesage <gefthefrench@gmail.com>
2017-09-16 11:14:21 -0500

    Added bitfinex trade monitoring

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 /* Dependencies */
 const Storage = require('./lib/storage');
+const Bitfinex = require('./lib/bitfinex');
 
 const storage = new Storage(
 {
-    dbId: process.env.DB_ID,
-    dbName: process.env.DB_NAME,
-    tableName: process.env.DB_TABLE,
-    credentials: process.env.DB_CREDS
+	dbId: process.env.DB_ID,
+	dbName: process.env.DB_NAME,
+	tableName: process.env.DB_TABLE,
+	credentials: process.env.DB_CREDS
 });
 
-storage.toString();
-
+Bitfinex(
+{
+	key: process.env.BIT_WS_KEY,
+	secret: process.env.BIT_WS_SECRET,
+	callback: storage.record
+});
